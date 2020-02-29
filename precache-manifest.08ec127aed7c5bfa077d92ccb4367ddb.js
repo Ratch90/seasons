10c0309781c519964a9d8bbc3d71c271d5b7d416
Ratch90 <Ratch90>
2020-03-01 00:53:59 +0100

    Updates

---
+++ precache-manifest.08ec127aed7c5bfa077d92ccb4367ddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a263435fea95456a46fbbc1708dee85",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "cba1fa661cd9a7490f25",
+    "url": "/seasons/static/css/main.ddfb87cf.chunk.css"
+  },
+  {
+    "revision": "d5ce41292cd93e9bfec0",
+    "url": "/seasons/static/js/2.c89fdf0a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/seasons/static/js/2.c89fdf0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba1fa661cd9a7490f25",
+    "url": "/seasons/static/js/main.29c01e65.chunk.js"
+  },
+  {
+    "revision": "42be413655da1659cb2e",
+    "url": "/seasons/static/js/runtime-main.a8b905c4.js"
+  }
+]);
