43745cfcf32e7abebfb533fcf92a8986ea4d88aa
Ratch90 <Ratch90>
2020-03-01 02:36:38 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonsDisplay'
+import Spinner from './Spinner'
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat : null,
+            errorMessage : ''
+        }
+    }
+
+    componentDidMount() {
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat : position.coords.latitude});
+            },
+            (err) => {
+                this.setState({errorMessage : err.message})
+            }
+        )
+        console.log('My component was rendered to the screen');
+    }
+
+    componentDidUpdate() {
+        console.log('My component was just updated - it rerendered');
+    }
+
+    renderContent () {
+
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error : {this.state.errorMessage}</div>
+        }
+         
+        else if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat = {this.state.lat}/>
+        }
+
+        else {
+            return <Spinner message = 'Please accept location request' />
+        }
+    }
+
+    render() {
+
+        return (
+            <div className='border red'>
+                {this.renderContent()}
+            </div>
+        )
+        
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
